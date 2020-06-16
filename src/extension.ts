2362d9ef83e1a5b83670aa69106e1fd0b146d1cd
Carlos J. Tur <carlosj.tur@outlook.com>
2020-06-16 21:11:31 +0200

    Solve some issues  getting the selected code

--- src/extension.ts
+++ src/extension.ts
@@ -5,26 +5,26 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposables = [];
 
 	disposables.push(vscode.commands.registerCommand('tinker-this.runSelection', async () => {
-		await vscode.commands.executeCommand('workbench.action.tasks.runTask', 'tinker-this: run');		
+		await vscode.commands.executeCommand('workbench.action.tasks.runTask', 'tinker-this: run');
 	}));
 
 	disposables.push(vscode.tasks.registerTaskProvider('tinker-this', {
 		provideTasks: () => {
-		  let command = getConsoleCommand();
-		  if(command === null) {
-			  vscode.window.showInformationMessage('The tinker command is not available');
-			  return undefined;
-		  }
-		  return [new vscode.Task(
-					  { type: "tinker-this", task: "run" },
-					  2,
-					  "run",
-					  'tinker-this',
-					  new vscode.ShellExecution(command)
-				  )];
+			let command = getConsoleCommand();
+			if (command === null) {
+				vscode.window.showInformationMessage('The tinker command is not available');
+				return undefined;
+			}
+			return [new vscode.Task(
+				{ type: "tinker-this", task: "run" },
+				2,
+				"run",
+				'tinker-this',
+				new vscode.ShellExecution(command)
+			)];
 		},
 		resolveTask(_task: vscode.Task): vscode.Task | undefined {
-		  return undefined;
+			return undefined;
 		}
 	}));
 
@@ -35,20 +35,23 @@ export function activate(context: vscode.ExtensionContext) {
 			return null;
 		}
 
-		return "echo '" + getSelectedCode()  + "'|" + php + ' artisan tinker';
+		return "echo '" + getSelectedCode() + "'|" + php + ' artisan tinker';
 	}
 
 	function getSelectedCode() {
 		const editor = vscode.window.activeTextEditor;
-			if(editor !== undefined) {
-				let selection = editor.selection;
-				let text = editor.document.getText(selection);
-				return text.replace(/\r?\n|\r|\ /g,'').replace(/'+/g, '"');
-			}
+		if (editor !== undefined) {
+			let selection = editor.selection;
+			let text = editor.document.getText(selection);
+			return text
+				.replace(/\r?\n/g, '')
+				.replace(/\ +/g, ' ')
+				.replace(/'+/g, '"');
+		}
 	}
 
 	context.subscriptions.push(...disposables);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
